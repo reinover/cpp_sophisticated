01ee56e7b5e536ef692a5202ca6d8036e92329af
reinover <reinover@gmail.com>
2021-10-11 22:26:57 +0300

    Update lesson1.cpp
    + first

--- lesson1/lesson1.cpp
+++ lesson1/lesson1.cpp
@@ -1,107 +1,161 @@
-﻿// Заглушка.
-// lesson1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-/*
-Имеется база сотрудников и номеров их телефонов. Требуется написать соответствующие структуры 
-для хранения данных, и заполнить контейнер записями из базы. Затем необходимо реализовать 
-методы обработки данных, а также вывести на экран всю необходимую информацию.
-Важно! Имена переменным, классам и функциям давайте осознанные, состоящие из слов на английском языке.
-
-1 Создайте структуру Person с 3 полями: фамилия, имя, отчество. Поле отчество должно быть опционального 
-типа, т.к. не у всех людей есть отчество. Перегрузите оператор вывода данных для этой структуры. 
-Также перегрузите операторы < и == (используйте tie).
-
-*/
-
-#include <iostream>
-#include <vector>
-#include <limits>
+﻿#include <iostream>
+#include <optional>
 #include <tuple>
-using namespace std;
+#include <iterator>
+#include <string>
+#include <vector>
+
+/* 1 */
+struct Person {
+	std::string first_name;
+	std::string second_name;
+	std::optional<std::string> last_name;
+};
+
+std::ostream& operator<<(std::ostream &out, struct Person &person) {
+	out << person.second_name << person.first_name << *person.last_name;
+	return out;
+}
 
 class Time {
 private:
 	int hour, minute, second;
 public:
-	Time(): hour(0), minute(0), second(0){}
+	Time() : hour(0), minute(0), second(0) {}
 	Time(int h, int m, int s) : hour(h), minute(m), second(s) {}
-	friend bool operator< (const Time& t1, const Time& t2);
+	friend bool operator<(const Time& t1, const Time& t2);
+	friend bool operator==(const Time& t1, const Time& t2);
 };
+
 bool operator<(const Time& t1, const Time& t2) {
-	/*if (t1.hour != t2.hour) {
-		return t1.hour < t2.hour;
-	}
-	if (t1.minute != t2.minute) {
-		return t1.minute < t2.minute;
-	}
-	return t1.second < t2.second;*/
-
-	// Альтернативное решение блога сверху через vector:
-	//return vector<int>{t1.hour, t1.minute, t1.second} < vector<int>{t2.hour, t2.minute, t2.second};
-
-	// Ещё один вариант через tie из tuple
-	return tie(t1.hour, t1.minute, t1.second) < tie(t2.hour, t2.minute, t2.second);
+	return std::tie(t1.hour, t1.minute, t1.second) < std::tie(t2.hour, t2.minute, t2.second);
 }
 
-int main()
-{
-	vector<int> numbers{ -5, -7, 3 };
-	int sum = 0;
-	for (auto n : numbers) {
-		sum += n;
+bool operator==(const Time& t1, const Time& t2) {
+	return std::tie(t1.hour, t1.minute, t1.second) == std::tie(t2.hour, t2.minute, t2.second);
+}
+
+/* 2 */
+
+struct PhoneNumber {
+	int city{};
+	int country{};
+	std::string number;
+	std::optional<int> addition_number;
+};
+
+std::ostream operator<<(std::ostream &out, struct PhoneNumber &pn) {
+	out << "+" << pn.country << "(" << pn.city << ")" << pn.number << *pn.addition_number;
+	// По какой то причине (пишет, что происходит обращение к удалённому конструктору) это строка выдаёт ошибку
+	// Хотя, по сути в операторе перегрузки для такой же структуры Person всё работает нормально.
+//    return out;
+}
+
+
+bool compare_by_names(std::tuple<std::string, std::string, std::string, long> first, std::tuple<std::string, std::string, std::string, long> second) {
+	return get<0>(first) < get<0>(second);
+}
+
+bool compare_by_phones(std::tuple<std::string, std::string, std::string, long> first, std::tuple<std::string, std::string, std::string, long> second) {
+	return get<3>(first) < get<3>(second);
+}
+
+std::tuple<std::string, long> GetPhoneNumber(std::string second_name, std::vector<std::tuple<std::string, std::string, std::string, long>> &address_book) {
+	int count = 0;
+	std::tuple<std::string, long> result = std::make_tuple("", 0);
+	std::for_each(address_book.begin(), address_book.end(), [&second_name, &count, &result](std::tuple<std::string, std::string, std::string, long> &t) {
+		if (second_name == get<0>(t)) {
+			get<0>(result) = get<0>(t);
+			get<1>(result) = get<3>(t);
+			count++;
+		}
+	});
+	if (count > 1) {
+		get<0>(result) = "found more than 1";
+		get<1>(result) = 0;
+		return result;
 	}
-	int average = sum / numbers.size();
-	cout << average << endl;
+	else if (count == 1) {
+		return result;
+	}
+	return std::make_tuple("not found", 0);
+}
 
-	cout << endl;
-	cout << "numeric_limits<size_t>::min() && numeric_limits<size_t>::max()  ::" << endl;
-	cout << "\t" << sizeof(size_t) << " bytes" << endl;
-	cout << "\tMin: " << numeric_limits<size_t>::min() << endl;
-	cout << "\tMax: " << numeric_limits<size_t>::max() << endl;
+void ChangePhoneNumber(std::vector<std::tuple<std::string, std::string, std::string, long>> &address_book) {
+	std::string human_name;
+	std::cout << "Введите имя человека: ";
+	std::cin >> human_name;
 
-	average = sum / static_cast<int>(numbers.size());
-	cout << endl;
-	cout << "С помощью static_cast преобразовали numbers.size() к типу int из size_t:" << endl;
-	cout << average << endl;
+	for (auto &ab : address_book) {
+		auto first_name = get<0>(ab);
+		if (first_name == human_name) {
+			long new_phone_num = 0;
+			std::cout << "Введите его новый номер: ";
+			std::cin >> new_phone_num;
+			get<3>(ab) = new_phone_num;
+		}
+	}
+}
 
-	// Кортежи
-	Time breakfast_time(9, 15, 0);
-	Time lunch_time(13, 0, 0);
-	// -- что такое boolalpha?
-	cout << boolalpha << (breakfast_time < lunch_time) << endl;
-
+int main() {
+	/* 1 */
 	{
-		// Создаём кортеж
-		tuple<int, string, double> tuple1(1, "Hello", 2.72);
-		auto tuple2 = make_tuple(2, "world", 3.14);
+		Person p;
+		p.first_name = "Sergey ";
+		p.second_name = "Nenashev ";
+		p.last_name = "Valerievich";
+		std::cout << p << std::endl;
 
-		// Получаем значения кортежа
-		auto hello = get<1>(tuple1);
-		auto world = get<1>(tuple2);
-		cout << world << endl;
-		// Странно, но нижеприведённый код не работает, cout справляется только если
-		// кортеж создан с помощью make_tuple
-		 //cout << hello << endl;
-		cout << get<0>(tuple1) << endl;
-
-		// Меняем значения кортежа
-		get<0>(tuple2) += get<0>(tuple1);
-		cout << get<0>(tuple2) << endl;
-
-		// Распаковка значений кортежа
-		int x;
-		string s;
-		double d;
-		tie(x, s, d) = tuple2;
-		// s отчего то не может вывести
-		cout << x << " " << d << endl;
-
-		// функция tie принимает только ссылки на объекты хранящиеся в других переменных.
-
-		auto students = {
-			make_tuple(234, "John Doe's", 3.7),
-			make_tuple(345, "Billy Foo's", 4.0),
-			make_tuple(456, "Cathy Bar's", 3.5),
-		};
+		Time time1(1, 1, 1);
+		Time time2(2, 2, 2);
+		Time time3(2, 2, 2);
+		std::cout << (time1 < time2) << std::endl;
+		std::cout << std::boolalpha << (time1 < time2) << std::endl;
+		std::cout << std::boolalpha << (time1 == time2) << std::endl;
+		std::cout << std::boolalpha << (time2 == time3) << std::endl;
 	}
+
+	/* 2 */
+	PhoneNumber pn;
+	pn.number = "123456";
+	pn.country = 7;
+	pn.city = 8182;
+
+	/* 3 */
+
+	std::tuple<std::string, std::string, std::string, long> human1("Иванов", "Иван", "Иванович", 7921123123123);
+	auto human2 = std::make_tuple("Петров", "Пётр", "Петрович", 7921098098098);
+	auto human3 = std::make_tuple("Сидоров", "Клим", "Александрович", 766609870987);
+	std::vector<std::tuple<std::string, std::string, std::string, long>> address_book;
+	address_book = {
+			human1,
+			human2,
+			human3
+	};
+
+	std::sort(address_book.begin(), address_book.end(), compare_by_names);
+	std::sort(address_book.begin(), address_book.end(), compare_by_phones);
+
+	for (auto ab : address_book) {
+		auto first_name = get<0>(ab);
+		auto second_name = get<1>(ab);
+		auto last_name = get<2>(ab);
+		auto phone = get<3>(ab);
+		std::cout << first_name << " " << second_name << " " << last_name << " " << phone << std::endl;
+	}
+
+	ChangePhoneNumber(address_book);
+
+	for (auto ab : address_book) {
+		auto first_name = get<0>(ab);
+		auto second_name = get<1>(ab);
+		auto last_name = get<2>(ab);
+		auto phone = get<3>(ab);
+		std::cout << first_name << " " << second_name << " " << last_name << " " << phone << std::endl;
+	}
+
+	auto gpn = GetPhoneNumber("Петров", address_book);
+	std::cout << get<0>(gpn) << " phone num: " << get<1>(gpn);
+
+	return 0;
 }
